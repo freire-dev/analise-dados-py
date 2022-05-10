c044b37d40afc466264e2ac2ec08e4c6a6d3c0f3
freire <luancfreire@gmail.com>
2022-05-10 00:16:30 -0300

    adicionando_mais_funcoes

--- main.py
+++ main.py
@@ -6,11 +6,23 @@ class Analise:
 
     def __init__ (self):
 
-        dialog1 = int(input("Olá, bem-vindo ao script sobre vendas de jogos. \n O que deseja saber? \n 1 - Vendas globais por plataforma \n"))
+        dialog1 = int(input("Olá, bem-vindo ao script sobre vendas de jogos. \n\n O que deseja saber? \n\n 1 - Total ($) em vendas globais por plataforma \n 2 - Média ($) em vendas globais por plataforma \n 3 - Total ($) em vendas globais por gênero de jogos \n 4 - Média ($) em vendas globais por gênero de jogos \n\n"))
         
         if(dialog1 == 1):
 
-            self.VendasPorPlataforma()
+            self.totalVendasPorPlataforma()
+
+        elif(dialog1 == 2):
+
+            self.mediaVendasPorPlataforma()
+
+        elif(dialog1 == 3):
+
+            self.totalVendasGenero()
+
+        elif(dialog1 == 4):
+
+            self.mediaVendasGenero()
 
         else:
 
@@ -24,20 +36,57 @@ class Analise:
 
                 print("Volte sempre!")
 
-    def VendasPorPlataforma (self):
+    def totalVendasPorPlataforma (self):
 
         dadosCSV = pd.read_csv("./vgsales.csv")
         dadosCSV = dadosCSV.dropna()
 
-        x = dadosCSV['Platform']
-        y = dadosCSV['Global_Sales']
-        
-        graficoBar = plt.bar(x, y, color="royalblue")
-        plt.title('Vendas por plataforma')
+        dadosVendas = dadosCSV[['Platform','Global_Sales']].groupby('Platform').sum()
+        dadosVendas.plot(kind='bar', color='royalblue')
         plt.xlabel('Plataforma')
-        plt.ylabel('Vendas')
-        plt.xticks(rotation=45)
+        plt.xticks(rotation=90)
+        plt.ylabel('Total em vendas')
+        plt.title('Total ($) de vendas por plataforma')
         plt.show()
-        plt.savefig('grafico_vendas.png')
+
+    def mediaVendasPorPlataforma (self):
+
+        dadosCSV = pd.read_csv("./vgsales.csv")
+        dadosCSV = dadosCSV.dropna()
+
+        dadosVendas = dadosCSV[['Platform','Global_Sales']].groupby('Platform').mean()
+        dadosVendas.plot(kind='bar', color='red')
+        plt.xlabel('Plataforma')
+        plt.xticks(rotation=90)
+        plt.ylabel('Média ($) em vendas')
+        plt.title('Média de vendas por plataforma')
+        plt.show()
+
+    def totalVendasGenero (self):
+
+        dadosCSV = pd.read_csv("./vgsales.csv")
+        dadosCSV = dadosCSV.dropna()
+
+        dadosVendas = dadosCSV[['Genre','Global_Sales']].groupby('Genre').sum()
+        dadosVendas.plot(kind='bar', color='green')
+        plt.xlabel('Gênero')
+        plt.xticks(rotation=90)
+        plt.ylabel('Total ($) em vendas')
+        plt.title('Total de vendas por gênero de jogos')
+        plt.show()
+
+    def mediaVendasGenero(self):
+
+        dadosCSV = pd.read_csv("./vgsales.csv")
+        dadosCSV = dadosCSV.dropna()
+
+        dadosVendas = dadosCSV[['Genre','Global_Sales']].groupby('Genre').mean()
+        dadosVendas.plot(kind='bar', color='purple')
+        plt.xlabel('Gênero')
+        plt.xticks(rotation=90)
+        plt.ylabel('Media ($) em vendas')
+        plt.title('Média de vendas por gênero de jogos')
+        plt.show()
+
 
 Analise() #Executar aplicação
